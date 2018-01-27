d2307bf3e00e4a6370ff9259a539ec9eab8cb596
Matthew Brookes <mbrookes1304@gmail.com>
2018-01-27 18:39:14 +0000

    Don't allow empty tag

--- options.js
+++ options.js
@@ -42,9 +42,11 @@ document.getElementById('save').addEventListener('click',
 //Defining a listener for our button, specifically, an onclick handler
 document.getElementById("add").onclick = function() {
     var text = document.getElementById("idea").value; //.value gets input values
-    optionList.push({text: text, checked: true});
-    renderoptionList();
-    save_options();
+    if (text) {
+      optionList.push({text: text, checked: true});
+      renderoptionList();
+      save_options();
+    }
 }
 
 function renderoptionList() {
